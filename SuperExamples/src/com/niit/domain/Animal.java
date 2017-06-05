2cde75afd46fe15492f78f23fbb71df995730465
yusufjk <yjkheraluwala@gmail.com>
2017-06-05 16:16:08 +0530

    Created a domain package and a Parent class Animal!!

---
+++ SuperExamples/src/com/niit/domain/Animal.java
@@ -0,0 +1,16 @@
+package com.niit.domain;
+
+public class Animal {
+	
+	//attribute
+	String name;
+	String bark;
+	
+	//constructor
+	public Animal(String n, String b)
+	{
+		name=n;
+		bark=b;
+	}
+
+}
